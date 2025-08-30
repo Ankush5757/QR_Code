ee56c44399d898048d83c4ee2cebc1bb777fa8fd
Ankush5757 <jadhavankush440@gmail.com>
2025-08-30 18:16:03 +0530

    Added Changes in the Views

--- App/views.py
+++ App/views.py
@@ -17,22 +17,25 @@ def generate_qr(request):
             url = form.cleaned_data['url']
             print(rest_name, url)
         
-            # Generate QR Code
+            # Generate QR Coode
             qr = qrcode.make(url)
             file_name = rest_name.replace(" ", "_").lower() + "_.png"
-            
-            # Convert to Base64
-            buffer = BytesIO()
-            qr.save(buffer, format='PNG')
-            qr_image_base64 = base64.b64encode(buffer.getvalue()).decode()
-            qr_url = f"data:image/png;base64,{qr_image_base64}"
-            
+            file_path = os.path.join(settings.MEDIA_ROOT, file_name) #media/rest_name_menu.png
+            qr.save(file_path)
+            context = {
+                'rest_name': rest_name,
+            }
+        
+
+            qr_url = os.path.join(settings.MEDIA_URL, file_name)
             context = { 
                 'rest_name': rest_name,
                 'qr_url': qr_url,
                 'file_name': file_name,
             }
+
             return render(request, 'Qr_Result.html', context)
+
     else:
         form = QRCodeForm()
         context = {'form': form}
